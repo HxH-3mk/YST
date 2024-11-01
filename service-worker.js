20e5dc25e4d4993f8e699951cbaeb53ac8301a91
S <58860531+HxH-3mk@users.noreply.github.com>
2024-11-02 02:07:01 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', function(event) {
   console.log('Service Worker installed');
+  // Activate the service worker immediately
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('Service Worker fetching', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
