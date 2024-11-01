3ced24b1b57124d65eff809253e06154a44a43c5
S <58860531+HxH-3mk@users.noreply.github.com>
2024-11-02 01:56:13 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Service Worker fetching', event.request.url);
+});
